098ca505af7cf017c24ccf919de21c9f9085a22f
biyuqi <biyuqiwan@163.com>
2017-10-16 11:21:39 +0800

    add comonents for vue todo

--- Vue/todo-app/src/App.vue
+++ Vue/todo-app/src/App.vue
@@ -2,9 +2,19 @@
   <div id="app">
     <!-- <router-view/> -->
     <h2 style="text-align:center;" class="active">TODO-LIST</h2>
+    <div class="box">
+        <i></i>
+        <i></i>
+        <i></i>
+    </div>
     <completed-todos></completed-todos>
     <get-todo></get-todo>
     <current-todos></current-todos>
+    <swiper-slide></swiper-slide>
+    <div class="" v-for="item in items" style="text-align:center">
+        <span>我是测试数据</span>
+    </div>
+    <back-top :visibleHeight="800" :rate="6"></back-top>
   </div>
 </template>
 
@@ -12,12 +22,21 @@
 import GetTodo from '@/components/todo/GetTodo'
 import CurrentTodos from '@/components/todo/CurrentTodos'
 import CompletedTodos from '@/components/todo/CompletedTodos'
+import BackTop from '@/components/BackTop.vue'
+import SwiperSlide from '@/components/SwiperSlide'
 export default {
   name: 'app',
   components: {
       GetTodo,
       CurrentTodos,
-      CompletedTodos
+      CompletedTodos,
+      BackTop,
+      SwiperSlide
+  },
+  data() {
+      return {
+          items:Array.from({length:100})
+      }
   }
 }
 </script>
@@ -28,6 +47,61 @@ export default {
     padding: 0;
     box-sizing: border-box;
 }
+.box{
+    width: 60px;
+    height: 60px;
+    margin: 0 auto;
+    /*background-color: #000;*/
+    position: absolute;
+    top: 12%;
+    left: 58%;
+    transform: translate(0%,0%);
+}
+.box i{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 2px solid #45dde7;
+    opacity: .8;
+    border-radius: 50%;
+    animation: loading 1.25s 0s infinite cubic-bezier(.21,.53,.56,.8);
+    /*cubic-bezier(.21,.53,.56,.8)*/
+}
+.box i:nth-child(1) {
+    animation-delay: 0s;
+    -webkit-animation-delay: 0s
+}
+
+.box i:nth-child(2) {
+    animation-delay: .2s;
+    -webkit-animation-delay: .2s
+}
+
+.box i:nth-child(3) {
+    animation-delay: .4s;
+    -webkit-animation-delay: .4s
+}
+@keyframes loading {
+    0% {
+        transform: scale(.1);
+        opacity: 1
+    }
+
+    70% {
+        transform: scale(1);
+        opacity: .7
+    }
+
+    100% {
+        opacity: 0
+    }
+}
+html,body{
+    height: 100%;
+    width: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -35,5 +109,8 @@ export default {
   /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
+  height: 100%;
+  position: relative;
+
 }
 </style>
---
+++ Vue/todo-app/src/components/SwiperSlide.vue
@@ -0,0 +1,83 @@
+<template lang="html">
+    <div class="swipper-wrap">
+        <transition-group tag='ul' class="clearfix slide" name='image'>
+            <li v-for='(item,index) in img' :key='index' v-show='index===mark'>
+                <a><img :src="item"></a>
+            </li>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            timer:null,
+            mark:0,
+            img: [
+                'http://upload-images.jianshu.io/upload_images/3360875-5625658440cb542d.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+                'http://upload-images.jianshu.io/upload_images/3360875-b70e9d81d26e2a27.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+                'http://upload-images.jianshu.io/upload_images/3360875-dc724649454c2ddc.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+                'http://upload-images.jianshu.io/upload_images/3360875-d2148a1bb7ea9d21.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+            ]
+        }
+    },
+    mounted() {
+        this.play()
+    },
+    methods: {
+        autoPlay() {
+            this.mark++
+            if(this.mark === this.img.length){
+                this.mark = 0
+                return;
+            }
+        },
+        play() {
+            this.timer = setInterval(()=>{
+                this.autoPlay()
+            },2500)
+        }
+    }
+}
+</script>
+
+<style lang="css">
+.swipper-wrap{
+    width: 800px;
+    height: 500px;
+    margin: 0 auto;
+    overflow: hidden;
+}
+ul{
+    width: 100%;
+    height: 400px;
+    position: relative;
+    list-style: none;
+}
+li{
+    position: absolute;
+    height: 400px;
+}
+li img{
+    width: 100%;
+    height: 100%;
+}
+.image-enter-active {
+    transform: translateX(0);
+    transition: all 1s ease-in-out;
+}
+
+.image-leave-active {
+    transform: translateX(-100%);
+    transition: all 1s ease-in-out;
+}
+
+.image-enter {
+    transform: translateX(100%)
+}
+
+.image-leave {
+    transform: translateX(0)
+}
+</style>
