6dc90a5f14cfd31e041400bb3a1ff53e3108eebf
biyuqi <biyuqiwan@163.com>
2017-11-20 16:47:34 +0800

    add some

--- 2017_BACK_UP_PROJECT/fileReader/filereader.js
+++ 2017_BACK_UP_PROJECT/fileReader/filereader.js
@@ -1,38 +1,51 @@
 !(function(window,undefined){
     "use strict";
     const ReadFileAndUpLoad = function(options){
-        let el
+        let el,
+            success = options.success;
         if(typeof options.el === 'string'){
             el = document.querySelector(options.el)
         }else{
             el = options.el
         }
-        const filesUlp = function(){
-            const files =this.files
-            const res = []
-            for(let i=0;i<files.length;i++){
-                let oFReader = new FileReader()
-                oFReader.onload = function(ev){
-                    res[i] =ev.target.result
-                    if(i === files.length-1){
-                        options.resolve([res,files])
-                    }
-                }
-                oFReader.readAsDataURL(files[i])
-            }
 
-            // options.imgs(res)
-            // options.files(files)
-        }
-        if(window.addEventListener){
-            el.addEventListener('change',filesUlp,false)
-        }else{
-            el.attachEvent('onchange',filesUlp,false)
+        return {
+            init: function(){
+                if(window.addEventListener){
+                    el.addEventListener('change',this.start,false)
+                }else{
+                    el.attachEvent('onchange',this.start,false)
+                }
+            },
+            start: function(){
+                const files =this.files
+                const res = []
+                let that = this
+                for(let i=0;i<files.length;i++){
+                    // let oFReader = new FileReader()
+                    // oFReader.onload = function(ev){
+                    //     res[i] =ev.target.result
+                    //     if(i === files.length-1){
+                    //         success && success(res,files)
+                    //     }
+                    // }
+                    // oFReader.readAsDataURL(files[i])
+                    var img = document.createElement('img')
+                    res.push(window.URL.createObjectURL(files[i]))
+                    if(i === files.length-1){
+                        success && success(res,files)
+                    }
+                    img.onload = function(){
+                        window.URL.revokeObjectURL(this.src)
+                        console.log(this.naturalWidth)
+                    }
+
+                }
+            }
         }
     }
-
     window.ReadFileAndUpLoad = function(options){
-        return new ReadFileAndUpLoad(options)
+        return new ReadFileAndUpLoad(options).init()
     }
 })(window);
 /**
