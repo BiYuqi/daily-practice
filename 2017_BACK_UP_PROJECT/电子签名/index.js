865c0fa3cadc580edcc47b0f87fd57b8108dd944
biyuqi <biyuqiwan@163.com>
2017-12-14 00:41:22 +0800

    电子签名ok

--- 2017_BACK_UP_PROJECT/电子签名/index.js
+++ 2017_BACK_UP_PROJECT/电子签名/index.js
@@ -1,4 +1,4 @@
-class Dom{
+class Utils{
     constructor() {
 
     }
@@ -19,17 +19,27 @@ class Dom{
     }
 
 }
-class Elect extends Dom{
-    constructor() {
+class Elect extends Utils{
+    constructor(canvas, degree) {
         super()
-        this.canvas = this.dom('#canvas')
-        this.ctx = this.canvas.getContext('2d')
+        this.degree = degree
+        /*************************/
+        let { width, height } = window.getComputedStyle(canvas, null);
+        width = width.replace('px', '');
+        height = height.replace('px', '');
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d')
         this.rect = this.getRealRect(this.canvas)
+        this.width = width;
+        this.height = height;
+        /*************************/
         // 优化高分屏
         this.resetCanvas()
+        this.resDregree()
         // 默认样式
         this.ctx.lineWidth = 6
         this.ctx.lineJoin = 'round'
+        this.ctx.lineCap = 'round'
         this.ctx.strokeStyle = 'black'
         /*************************************/
         // 下载的时候防止黑背景
@@ -48,15 +58,13 @@ class Elect extends Dom{
             this.ctx.shadowColor = 'black'
         }
         this.bindEvent()
-
     }
 
     resetCanvas() {
         const { canvas, ctx, rect } = this
-
         const DPR = this.DPR()
-        const width = rect.width
-        const height = rect.height
+        const width = this.width
+        const height = this.height
 
         canvas.width = width * DPR
         canvas.height = height * DPR
@@ -96,13 +104,34 @@ class Elect extends Dom{
                 this.point.x = x - rect.left + 0.5
                 this.point.y = y - rect.top + 0.5
                 this.drawLine(sign)
-
                 e.preventDefault()
             }
         }
         return setParam
     }
 
+    resDregree() {
+        const { degree } = this
+        if (typeof degree == 'number') {
+            this.ctx.rotate((degree * Math.PI) / 180)
+            let height = this.height
+            let width = this.width
+
+            switch (degree) {
+                case -90:
+                    this.ctx.translate(-height, 0);
+                    break;
+                case 90:
+                    this.ctx.translate(0, -width);
+                    break;
+                case -180:
+                case 180:
+                    this.ctx.translate(-width, -height);
+                    break;
+                default:
+            }
+        }
+    }
     bindEvent() {
         const start = this.createAction(1)
         const move = this.createAction(2)
@@ -119,12 +148,6 @@ class Elect extends Dom{
         }
 
         this.dom('button').addEventListener('click',()=>{
-
-            // var img = document.createElement('img')
-            // img.src = this.canvas.toDataURL('image/jpeg')
-            // img.width = this.rect.width
-            // img.height = this.rect.height
-            // document.body.appendChild(img)
             function downLoad(data,filename = Date.now()){
                 const a = document.createElement('a')
                 a.href = data.toDataURL('image/jpeg')
@@ -135,6 +158,56 @@ class Elect extends Dom{
         })
     }
 
-
 }
-new Elect()
+
+
+class MakeTest extends Utils{
+    constructor() {
+        super()
+        new Elect(this.dom('canvas'), 0)
+        this.test()
+    }
+
+    test() {
+        const select = this.dom('.select')
+        select.addEventListener('change',()=>{
+            let degree = ~~select.value
+            const w = window.innerWidth || d.documentElement.clientWidth || d.body.clientWidth
+            const h = window.innerHeight || d.documentElement.clientHeight || d.body.clientHeight
+            let width = w
+            let height = h
+            let length = (h - w) / 2;
+            switch (degree) {
+                case -90:
+                    length = -length;
+                case 90:
+                    width = h;
+                    height = w;
+                    break;
+                default:
+                    length = 0;
+            }
+            const styles = {
+                transform: `rotate(${degree}deg) translate(${length}px,${length}px)`,
+                width: `${width}px`,
+                height: `${height}px`,
+                transformOrigin: 'center center',
+            }
+
+            if(this.dom('.canvas-wrapper')){
+                this.dom('.canvas-wrapper').removeChild(document.querySelector('canvas'));
+                this.dom('.canvas-wrapper').appendChild(document.createElement('canvas'));
+                setTimeout(() => {
+                  new Elect(this.dom('canvas'), -degree)
+                }, 200);
+            }
+
+            for(let i in styles){
+                this.dom('.canvas-wrapper').style[i] = styles[i]
+            }
+
+        })
+    }
+}
+
+new MakeTest()
