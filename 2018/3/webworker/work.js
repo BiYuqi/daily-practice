2c20d485d2d222bc63ed1c873332ea2a9b132084
biyuqi <biyuqiwan@163.com>
2018-03-04 19:27:22 +0800

    add worker

---
+++ 2018/3/webworker/work.js
@@ -0,0 +1,10 @@
+self.onmessage = (ev) => {
+  let num = ev.data
+  const inter = setInterval(() => {
+    self.postMessage(--num)
+    if (num <= 0) {
+      clearInterval(inter)
+      // self.close()
+    }
+  }, 1000)
+}
