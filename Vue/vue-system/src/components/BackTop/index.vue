0d5136828b14cd416472d02e5fdfc0c18f943ec4
biyuqi <biyuqiwan@163.com>
2018-03-11 19:38:44 +0800

    add backtotop

--- Vue/vue-system/src/components/BackTop/index.vue
+++ Vue/vue-system/src/components/BackTop/index.vue
@@ -1,10 +1,10 @@
 <template lang="html">
   <div class="b-back-top" @click="goTop" name="button" v-show="visible" :style="customStyle">
     <svg width="16" height="16" viewBox="0 0 17 17" xmlns="http://www.w3.org/2000/svg" class="back-icon" aria-hidden="true" style="height: 16px; width: 16px;">
-        <g>
-          <path d="M12.036 15.59c0 .55-.453.995-.997.995H5.032c-.55 0-.997-.445-.997-.996V8.584H1.03c-1.1 0-1.36-.633-.578-1.416L7.33.29c.39-.39 1.026-.385 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.004z" fill-rule="evenodd"></path>
-        </g>
-      </svg>
+      <g>
+        <path d="M12.036 15.59c0 .55-.453.995-.997.995H5.032c-.55 0-.997-.445-.997-.996V8.584H1.03c-1.1 0-1.36-.633-.578-1.416L7.33.29c.39-.39 1.026-.385 1.412 0l6.878 6.88c.782.78.523 1.415-.58 1.415h-3.004v7.004z" fill-rule="evenodd"></path>
+      </g>
+    </svg>
   </div>
 </template>
 
@@ -15,7 +15,7 @@ export default {
       type: Number,
       default: 400
     },
-    rate: {
+    rate: { // 速率
       type: Number,
       default: 6
     },
